cfb0c845b0509c8b3cb0152726e0aea78af47bba
Matthew <2565361+mshafer1@users.noreply.github.com>
2023-08-19 09:48:09 -0500

    get it loading the config sheet

--- student_teacher_gradebook/__init__.py
+++ student_teacher_gradebook/__init__.py
@@ -26,10 +26,24 @@ def _openWorkbook(xlapp, xlfile):
     return xlwb
 
 
+def _to_snake_case(value: str) -> str:
+    """Split value on words and make snake case
+
+    >>> _to_snake_case("Student Template Filename")
+    'student_template_filename'
+    """
+    return "_".join(x.lower() for x in value.split())
+
+
+class _Config(typing.NamedTuple):
+    student_template_filename: str
+    student_filename_format_string: str
+
+
 class _MainWorkbook(_BaseWorkBook):
     def __init__(self, path: pathlib.Path) -> None:
         super().__init__(path)
-        self._app = win32.gencache.EnsureDispatch('Excel.Application')
+        self._app = win32.gencache.EnsureDispatch("Excel.Application")
         self._app.Visible = True
         self._workbook = None
         self._config = None
@@ -49,7 +63,28 @@ class _MainWorkbook(_BaseWorkBook):
     def _load_config(self):
         student_name_to_workbook_mapping = {}
         config_sheet = self._workbook.Worksheets(_config.CONFIG_SHEET_NAME)
-        
+        max_rows = config_sheet.UsedRange.Rows.Count
+
+        _config_keys = _Config._fields
+
+        config_sheet_data = {}
+        for i in range(1, max_rows + 1):
+            key = (config_sheet.Cells(i, 1).Value or "").rstrip(":")
+            value = config_sheet.Cells(i, 2).Value
+            if not key:
+                continue
+            key = _to_snake_case(key)
+            if key in _config_keys:
+                config_sheet_data[key] = value
+        try:
+            self._config = _Config(**config_sheet_data)
+        except TypeError:
+            _MODULE_LOGGER.warning(
+                "Error, all required keys must be provided on the %s sheet.",
+                _config.CONFIG_SHEET_NAME,
+            )
+            raise
+
     def __enter__(self):
         self._workbook = _openWorkbook(self._app, str(self._path))
         self._load_config()
