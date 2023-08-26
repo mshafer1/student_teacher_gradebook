cbecb9be437adb49bd2d54d92bda58a81aec111a
Matthew <2565361+mshafer1@users.noreply.github.com>
2023-08-26 08:10:37 -0500

    format

--- tests/component/update_student_sheets/test_update_student_sheets.py
+++ tests/component/update_student_sheets/test_update_student_sheets.py
@@ -30,16 +30,18 @@ def test_case_with_temp_cwd(test_case: pathlib.Path, temp_cwd: pathlib.Path):
     yield test_case
     student_teacher_gradebook._config.TEACHER_BOOK = old_value
 
+
 @pytest.fixture()
 def test_id_as_file(temp_cwd: pathlib.Path, request):
     (temp_cwd / request.node.callspec.id).touch()
+
 
 def test____update_student_sheets___component(
     test_case_with_temp_cwd: pathlib.Path,
     temp_cwd: pathlib.Path,
     console_runner: tests._utils.RUNNER_TYPE,
     snapshot: pytest_snapshot.plugin.Snapshot,
-    test_id_as_file
+    test_id_as_file,
 ):
     result = console_runner("update-student-sheets")
 
